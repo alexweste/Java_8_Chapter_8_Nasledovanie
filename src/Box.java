913c6342732955932a1e700f81a0bc1605c5f536
alexweste <alexandrzvarygin@gmail.com>
2018-05-06 20:01:07 +0300

    Применим наследование для расширения класса Box, добавив в него параметр weight

--- src/Box.java
+++ src/Box.java
@@ -48,10 +48,25 @@ class BoxWeight extends Box {
     }
 }
 
+// Расширяем класс Box, включая в него свойство цвета
+class ColorBox extends Box{
+    char color; // вводим переменную, отвечающую за цвет параллелепипеда
+
+    // создаём конструктор ColorBox
+    ColorBox(double w, double h, double d, char c){
+        width = w;
+        height = h;
+        depth = d;
+        color = c;
+    }
+}
 class DemoBoxWeight{
     public static void main(String[] args){
         BoxWeight mybox1 = new BoxWeight(10, 25, 30,4.4);
         BoxWeight mybox2 = new BoxWeight(7, 6, 5,9.0028);
+
+        ColorBox mybox3 = new ColorBox(15, 23, 35, 'g');
+        ColorBox mybox4 = new ColorBox(17, 65, 16,'b');
         double vol;
 
         vol = mybox1.volume();
@@ -63,5 +78,15 @@ class DemoBoxWeight{
         System.out.println("Объём mybox2 равен " + vol);
         System.out.println("Вес mybox2 равен " + mybox2.weight);
         System.out.println();
+
+        vol = mybox3.volume();
+        System.out.println("Объём mybox3 равен " + vol);
+        System.out.println("Цвет mybox3 равен " + mybox3.color);
+        System.out.println();
+
+        vol = mybox4.volume();
+        System.out.println("Объём mybox4 равен " + vol);
+        System.out.println("Цвет mybox4 равен " + mybox4.color);
+        System.out. println();
     }
 }
